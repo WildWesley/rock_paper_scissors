5b8b7031e8a566bbc93676af3f78a5563b4cc77c
WildWesley <hillspirit2021@gmail.com>
2025-04-12 19:35:40 -0600

    Add functionality to press buttons and keep score

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,5 +1,3 @@
-const prompt = require("prompt-sync")();
-
 function getComputerChoice() {
     let computer_choice = null;
     let random = Math.random();
@@ -12,37 +10,123 @@ function getComputerChoice() {
     }
     return computer_choice
 }
+let computer_choice = null;
+let human_choice = null;
+let computer_choice_text = null;
+let human_choice_text = null;
+let round_choice_text = null;
+let human_score = 0;
+let computer_score = 0;
+const Rock = document.getElementById("Rock");
+const Paper = document.getElementById("Paper");
+const Scissors = document.getElementById("Scissors");
+const Human_Score = document.getElementById("Human-Score");
+const Computer_Score = document.getElementById("Computer-Score");
 
-function getHumanChoice() {
-    return prompt("rock, paper, or scissors: ")
-}
+Rock.addEventListener("click", () => {
+    computer_choice = getComputerChoice();
+    human_choice = "rock";
+    computer_choice_text = document.getElementById("Computer-Choice-Text");
+    human_choice_text = document.getElementById("Human-Choice-Text");
+    round_choice_text = document.getElementById("Round-Result-Text");
+    if (human_score < 5 && computer_score < 5) {
+        human_choice_text.textContent = "You Chose Rock...";
+        if (computer_choice === "scissors") {
+            computer_choice_text.textContent = "Rob Chose Scissors...";
+            round_choice_text.textContent = "You Won The Round!";
+            human_score += 1;
+            Human_Score.textContent = `You: ${human_score}`;
+        } else if (computer_choice === "paper") {
+            computer_choice_text.textContent = "Rob Chose Paper...";
+            round_choice_text.textContent = "You Lost The Round...";
+            computer_score += 1;
+            Computer_Score.textContent = `Computer: ${computer_score}`;
+        } else {
+            computer_choice_text.textContent = "Rob Chose Rock...";
+            round_choice_text.textContent = "It's A Tie!";
+        }
+    } else {
+        if (human_score === 5) {
+            Human_Score.textContent = "You Won!!!";
+            Computer_Score.textContent = "";
+        } else {
+            Human_Score.textContent = "Get Cooked Kid.";
+            Computer_Score.textContent = "";
+        };
+    }
+});
 
-let computer_choice = getComputerChoice();
-let human_choice = getHumanChoice();
-if (human_choice === "rock") {
-    if (computer_choice === "scissors") {
-        console.log("Computer chose scissors! You win!");
-    } else if (computer_choice === "paper") {
-        console.log("Computer chose paper. You lose.");
+
+Paper.addEventListener("click", () => {
+    computer_choice = getComputerChoice();
+    human_choice = "paper";
+    computer_choice_text = document.getElementById("Computer-Choice-Text");
+    human_choice_text = document.getElementById("Human-Choice-Text");
+    round_choice_text = document.getElementById("Round-Result-Text");
+
+    if (human_score < 5 && computer_score < 5) {
+        human_choice_text.textContent = "You Chose Paper...";
+        if (computer_choice === "scissors") {
+            computer_choice_text.textContent = "Rob Chose Scissors...";
+            round_choice_text.textContent = "You Lost The Round...";
+            computer_score += 1;
+            Computer_Score.textContent = `Computer: ${computer_score}`;
+        } else if (computer_choice === "paper") {
+            computer_choice_text.textContent = "Rob Chose Paper...";
+            round_choice_text.textContent = "It's A Tie!";
+        } else {
+            computer_choice_text.textContent = "Rob Chose Rock...";
+            round_choice_text.textContent = "You Won The Round!";
+            human_score += 1;
+            Human_Score.textContent = `You: ${human_score}`;
+        }
     } else {
-        console.log("Computer chose rock as well! It's a tie!");
+        if (human_score === 5) {
+            Human_Score.textContent = "You Won!!!";
+            Computer_Score.textContent = "";
+        } else {
+            Human_Score.textContent = "Get Cooked Kid.";
+            Computer_Score.textContent = "";
+        };
     }
-} else if (human_choice === "paper") {
-    if (computer_choice === "rock") {
-        console.log("Computer chose rock! You win");
-    } else if (computer_choice === "scissors") {
-        console.log("Computer chose scissors. You lose");
+});
+
+Scissors.addEventListener("click", () => {
+    computer_choice = getComputerChoice();
+    human_choice = "scissors";
+    computer_choice_text = document.getElementById("Computer-Choice-Text");
+    human_choice_text = document.getElementById("Human-Choice-Text");
+    round_choice_text = document.getElementById("Round-Result-Text");
+
+    if (human_score < 5 && computer_score < 5) {
+        human_choice_text.textContent = "You Chose Scissors...";
+        if (computer_choice === "scissors") {
+            computer_choice_text.textContent = "Rob Chose Scissors...";
+            round_choice_text.textContent = "It's A Tie!";
+        } else if (computer_choice === "paper") {
+            computer_choice_text.textContent = "Rob Chose Paper...";
+            round_choice_text.textContent = "You Won The Round!";
+            human_score += 1;
+            Human_Score.textContent = `You: ${human_score}`;
+        } else {
+            computer_choice_text.textContent = "Rob Chose Rock...";
+            round_choice_text.textContent = "You Lost The Round...";
+            computer_score += 1;
+            Computer_Score.textContent = `Computer: ${computer_score}`;
+        }
     } else {
-        console.log("Computer chose paper as well! It's a tie!");
+        if (human_score === 5) {
+            Human_Score.textContent = "You Won!!!";
+            Computer_Score.textContent = " ";
+        } else {
+            Human_Score.textContent = "Get Cooked Kid.";
+            Computer_Score.textContent = " ";
+        };
     }
-} else if (human_choice === "scissors") {
-    if (computer_choice === "paper") {
-        console.log("Computer chose paper! You win!");
-    } else if (computer_choice === "rocke") {
-        console.log("Computer chose rock. You lose");
-    } else {
-        console.log("Computer also chose scissors! It's a tie!")
-    }
-} else {
-    console.log("Invalid choice! Please choose rock, paper, or scissors!");
-}
+});
+
+
+/* I'll have to have a collect function that takes in the user's input depending on button pressed */
+/* Then, I'll replace the logic gates that I have set up to change the heading texts instead of loggin to console */
+/* I'll add a score feature that updates the score depending on who wins */
+/* I'll add a game end that displays the overall winner for first to 5 */
